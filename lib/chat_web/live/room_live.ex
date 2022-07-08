e9facdc4fbe64f5698b62e362b7fec6f4ebfdba3
geetika1723 <geetikasethi1723@gmail.com>
2022-07-08 14:35:18 +0530

    updated

---
+++ lib/chat_web/live/room_live.ex
@@ -0,0 +1,43 @@
+defmodule ChatWeb.RoomLive do
+  use ChatWeb, :live_view
+  require Logger
+
+  @impl true
+  def mount(%{"id"=> room_id}, _session, socket) do
+    topic = "room: " <> room_id
+    #username=MnemonicSlugs.generate_slug(2)
+    if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+      assign(socket,
+      room_id: room_id,
+       topic: topic,
+       #username: username,
+       message: "",
+        messages: [%{uuid: UUID.uuid4(), content: "Twitch joined chat!" #username: "system"
+        }],
+        temporary_assigns: [messages: []]
+        )}
+  end
+
+  @impl true
+  def handle_event("submit_message" , %{"chat" => %{"message" => message}}, socket) do
+
+   message = %{uuid: UUID.uuid4(),content: message #, username: socket.assigns.username
+  }
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, assign(socket, message: "")}
+  end
+
+  @impl true
+  def handle_event("form_update", %{"chat" => %{"message" => message}}, socket) do
+    Logger.info(message: message)
+    {:noreply, assign(socket, message: message)}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message},socket) do
+
+      {:noreply, assign(socket,messages: [message])}
+  end
+end
